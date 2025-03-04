bed71e1a20335d2c9d654d281ec1b9eb81576a31
HalynaKomar <124254423+HalynaKomar@users.noreply.github.com>
2025-03-04 18:18:29 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,35 +25,35 @@ export default function RootLayout({ children }) {
                 </div>
 
                 
-<script async src="https://stage-widget.intelswift.com/script.js?tenantId=835c3375-8bff-4f6b-94d0-dbbd537f5303&botId=67c6f37e01b3e636a11e4eed&uuid=70907f9c-aaec-43dc-a0d4-7d82108c376a&end=true"></script>
+<script async src="https://stage-widget.intelswift.com/script.js?tenantId=bec61ef5-7901-438d-a42a-fc3cd68d5497&botId=67c726f901b3e636a11e5751&uuid=1d17d3f6-dfaf-44a6-a8a1-22b8be395dc9&end=true"></script>
 <script
     dangerouslySetInnerHTML={{
         __html: `
-            window.onload = function(event) {
-                const propsInterval = setInterval(widgetTimer, 1000);
+            window.onload = (event) => {
+    const propsInterval = setInterval(widgetTimer, 1000);
 
-                function widgetTimer() {
-                    const tenantId = localStorage.getItem("wws-tenant-id");
-                    const botId = localStorage.getItem("wws-bot-id");
-                    const uuid = localStorage.getItem("wws-uuid");
-                    const host = window.location.hostname;
-                    const language = navigator.language || navigator.userLanguage; 
+    function widgetTimer() {
+      const tenantId = localStorage.getItem("wws-tenant-id")
+      const botId = localStorage.getItem("wws-bot-id")
+      const uuid = localStorage.getItem("wws-uuid")
+      const host = window.location.hostname
+      const language = navigator.language || navigator.userLanguage; 
 
-                    if (
-                        (uuid && uuid !== "undefined") && 
-                        (tenantId && tenantId !== "undefined") && 
-                        (botId && botId !== "undefined") && 
-                        (host && host !== "undefined")
-                    ) {
-                        clearInterval(propsInterval);
-                        document.getElementById("iframeWidgetContainer").contentWindow.postMessage(
-                            {
-                                tenantId: tenantId,
-                                botId: botId,
-                                uuid: uuid,
-                                host: host,
-                                contact_language: language
-                            }, "*"
+      if(
+        (uuid && uuid != "undefined") && 
+        (tenantId && tenantId != "undefined") && 
+        (botId && botId != "undefined") && 
+        (host && host != "undefined")
+      ){
+        clearInterval(propsInterval);
+        document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+        {
+          tenantId: tenantId,
+          botId: botId,
+          uuid: uuid,
+          host: host,
+          contact_language: language
+        },"*")
                         );
                     }
                 }
