aca59cdbd698c2bae5c77d436e2d5eba4b08c8e8
HalynaKomar <124254423+HalynaKomar@users.noreply.github.com>
2025-03-08 11:24:31 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,41 +22,38 @@ export default function RootLayout() {
                 />
 
                 {/* Інтеграція скриптів */}
-                <script
-                    async
-                    src="https://stage-widget.intelswift.com/script.js?tenantId=edd30677-c5a4-4db7-a9e2-d2b7076a3e07&botId=67cbfc9aca1eece27c50ca96&uuid=a82cdc57-4b9c-4806-a29b-392d1d3ddf6d&end=true"
-                ></script>
-
-                {/* Вставка JS-коду у JSX через dangerouslySetInnerHTML */}
+<script async src="https://stage-widget.intelswift.com/script.js?tenantId=aa9944db-2524-4912-be6c-e0024673a65e&botId=67cad9cd6381d68d8e0e8605&uuid=cc5c4888-ecd9-4996-ac21-bcad4c733f47&end=true"></script>               
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                          window.onload = (event) => {
-                              const propsInterval = setInterval(widgetTimer, 1000);
+                             window.onload = (event) => {
+    const propsInterval = setInterval(widgetTimer, 1000);
 
-                              function widgetTimer() {
-                                  const tenantId = localStorage.getItem("wws-tenant-id");
-                                  const botId = localStorage.getItem("wws-bot-id");
-                                  const uuid = localStorage.getItem("wws-uuid");
-                                  const host = window.location.hostname;
-                                  const language = navigator.language || navigator.userLanguage; 
+    function widgetTimer() {
+      const tenantId = localStorage.getItem("wws-tenant-id")
+      const botId = localStorage.getItem("wws-bot-id")
+      const uuid = localStorage.getItem("wws-uuid")
+      const host = window.location.hostname
+      const language = navigator.language || navigator.userLanguage; 
 
-                                  if (
-                                      (uuid && uuid !== "undefined") && 
-                                      (tenantId && tenantId !== "undefined") && 
-                                      (botId && botId !== "undefined") && 
-                                      (host && host !== "undefined")
-                                  ) {
-                                      clearInterval(propsInterval);
-                                      document.getElementById("iframeWidgetContainer").contentWindow.postMessage({
-                                          tenantId: tenantId,
-                                          botId: botId,
-                                          uuid: uuid,
-                                          host: host,
-                                          contact_language: language
-                                      }, "*");
-                                  }
-                              }
+      if(
+        (uuid && uuid != "undefined") && 
+        (tenantId && tenantId != "undefined") && 
+        (botId && botId != "undefined") && 
+        (host && host != "undefined")
+      ){
+        clearInterval(propsInterval);
+        document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+        {
+          tenantId: tenantId,
+          botId: botId,
+          uuid: uuid,
+          host: host,
+          contact_language: language
+                                        }, "*"
+                                    );
+                                }
+                            }
                           };
                         `,
                     }}
