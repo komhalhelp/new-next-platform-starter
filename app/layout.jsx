5d24025dafca35df28128c2a76c41871792ba82e
HalynaKomar <124254423+HalynaKomar@users.noreply.github.com>
2025-03-24 12:43:30 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout() {
 
                 {/* Інтеграція скриптів */}
 
-<script async src="https://stage-widget.intelswift.com/script.js?tenantId=526bfbbb-98a6-4cc2-87b0-edff222fdaa1&botId=67dc2f9fe0571ec8772d221d&uuid=7bba90d8-1b9d-40f9-83ff-039e6ec6ef1a&end=true"></script>   
+<script async src="https://stage-widget.intelswift.com/script.js?tenantId=526bfbbb-98a6-4cc2-87b0-edff222fdaa1&botId=67e13732e0571ec8772d43ec&uuid=7bba90d8-1b9d-40f9-83ff-039e6ec6ef1a&end=true"></script>       
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
