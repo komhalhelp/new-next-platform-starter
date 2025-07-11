d88fccb59176d14ab042379296f311e1bbe3895c
halynakomar4 <halynakomar4@gmail.com>
2025-07-11 13:47:02 +0300

    Update layoutAnna_prod.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,65 +1,58 @@
 import '../styles/globals.css';
 
 export const metadata = {
-    title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
-    }
+  title: {
+    template: '%s | Netlify',
+    default: 'Netlify Starter',
+  },
 };
 
 export default function RootLayout() {
-    return (
-        <html lang="en" data-theme="lofi">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="w-screen h-screen flex items-center justify-center">
-                {/* Image */}
+  return (
+    <html lang="en" data-theme="lofi">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
+               <body className="w-screen h-screen flex items-center justify-center">
+                {/* Картинка на весь екран */}
                 <img
                     src="/images/image.jpg"
                     alt="Full Screen Image"
                     className="w-full h-full object-cover"
                 />
 
-                {/* Integration of script  */}
-
-
-<script async src="https://widget.intelswift.com/script.js?tenantId=094f2d86-c31e-4a4c-b5b5-c890ee26399a&botId=6801f34f2c2f8c95aacb3769&uuid=90af0a1a-435e-4151-a8d5-dd3e3457d8fc&end=true"></script>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                             window.onload = (event) => {
-    const propsInterval = setInterval(widgetTimer, 1000);
-
-    function widgetTimer() {
-      const tenantId = localStorage.getItem("wws-tenant-id")
-      const botId = localStorage.getItem("wws-bot-id")
-      const uuid = localStorage.getItem("wws-uuid")
-      const host = window.location.hostname
-      const language = navigator.language || navigator.userLanguage; 
-
-      if(
-        (uuid && uuid != "undefined") && 
-        (tenantId && tenantId != "undefined") && 
-        (botId && botId != "undefined") && 
-        (host && host != "undefined")
-      ){
-        clearInterval(propsInterval);
-        document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
-        {
-          tenantId: tenantId,
-          botId: botId,
-          uuid: uuid,
-          host: host,
-          contact_language: language
-        },"*")
-      }
-    }
-  };
-                        `,
-                    }}
-                />
-            </body>
-        </html>
-    );
+        {/* Intelswift Messenger */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.__INTELSWIFT_MESSENGER_CONFIG__ = {
+                customer_language: navigator.language || "en"
+              };
+            `,
+          }}
+        />
+        <script
+    async
+	src="https://widget.intelswift.com/script.js"
+	data-tenant-id="ef7f7c92-1d7f-4ef7-86fa-bf437b419def"
+	data-bot-id="6870e335b4b419b587b652b6"
+	data-assistant-id="6870e31cb4b419b587b65289"
+	data-uuid="efd87bb5-f483-478e-ba57-3b02820c01f9"
+	data-sandbox="false"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.addEventListener("message", (event) => {
+                if (event.data?.type === "messengerReady") {
+                  const iframe = document.getElementById("iFrameMessenger");
+                  iframe?.contentWindow.postMessage({ type: "verifyOrigin", href: window.location.href }, "*");
+                }
+              });
+            `,
+          }}
+        />
+      </body>
+    </html>
+  );
 }
